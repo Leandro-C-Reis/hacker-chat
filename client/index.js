a7af35cac260a45413a4aff16384a2ad3432614f
leandro-c-reis <leandroreis02@gmail.com>
2021-03-25 09:42:13 -0300

    feat(client): Client socket connection

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import Events from 'events';
 import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 import TerminalController from "./src/termialController.js";
 
 const [nodePath, filePath, ...commands] = process.argv;
@@ -8,6 +9,8 @@ const config = CliConfig.parseArguments(commands);
 async function index()
 {   
     const componentEmitter = new Events();
+    const socketClient = new SocketClient(config);
+    await socketClient.initialize();
     
     const controller = new TerminalController();
     await controller.initializeTable(componentEmitter);
